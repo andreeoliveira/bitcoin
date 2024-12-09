5f9c4764bf9ee4d64d7008e10cf491762c307cbd
Andre Oliveira <andreducmk@gmail.com>
2024-12-09 16:50:54 +0000

    Update getcoins.py

--- contrib/signet/getcoins.py
+++ contrib/signet/getcoins.py
@@ -129,7 +129,7 @@ if args.captcha != '': # Retrieve a captcha
 
     # Convert SVG image to PPM, and load it
     try:
-        rv = subprocess.run([args.imagemagick, 'svg:-', '-depth', '8', 'ppm:-'], input=res.content, check=True, capture_output=True)
+        rv = shutil.which(args.imagemagick)
     except FileNotFoundError:
         raise SystemExit(f"The binary {args.imagemagick} could not be found. Please make sure ImageMagick (or a compatible fork) is installed and that the correct path is specified.")
 
